43862055bc90fe016f93b9c810fb413e74b1e8a4
miracle-ic <miracle3cle@gmail.com>
2023-06-26 01:10:28 +0100

    4th commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,16 @@
-
+import React from 'react'
 import './App.css'
 import Usernav from './component/Navbar/Usernav'
 import Sidebar from './component/Sidebar/Sidebar'
+import Userprofile from './component/Userprofile/Userprofile'
 
 function App() {
 
   return (
     <>
       {/* <Usernav /> */}
-      <Sidebar />
+      {/* <Sidebar /> */}
+      <Userprofile />
     </>
   )
 }
